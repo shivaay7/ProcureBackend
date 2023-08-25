09a7ce1c35f3ba712f0d97f407cb6ca946a6d8e4
Shivam Mishra <shivammishra7613@gmail.com>
2023-08-25 20:47:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,20 +18,32 @@ connectDB();
 const app = express();
 
 //middlewares
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://procurefrontend.onrender.com'
+}));
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://procurefrontend.onrender.com');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 //routes
 app.use('/api/v1/auth', authRoutes)
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
+
 //rest api
 app.get('/', (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials","true")
     res.send("<h1>Welcome to My Ecommerce App</h1>")
 })
+
+
 //port 
 const PORT = process.env.PORT;
 
